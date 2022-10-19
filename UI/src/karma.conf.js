104c81dfe54e111140d66ccde7d82075f65d29d5
Bhagyashree Shettigar <bhashett1@publicisgroupe.net>
2022-10-19 16:01:00 +0530

    DTS-19793 removed puppeteer and gzipper dependencies , updated gitignore file

--- UI/src/karma.conf.js
+++ UI/src/karma.conf.js
@@ -2,8 +2,6 @@
 // https://karma-runner.github.io/1.0/config/configuration-file.html
 
 
-const puppeteer = require('puppeteer');
-process.env.CHROME_BIN = puppeteer.executablePath();
 
 module.exports = function (config) {
   config.set({
