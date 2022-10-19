4777576609993165a64645102ed2be69e2d9f98d
hirbabar <hirbabar@publicisgroupe.net>
2022-10-19 18:31:34 +0530

    multiple board configure in jira

---
+++ customapi/src/main/java/com/publicissapient/kpidashboard/apis/jira/service/JiraToolConfigServiceImpl.java
@@ -0,0 +1,109 @@
+package com.publicissapient.kpidashboard.apis.jira.service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import org.bson.types.ObjectId;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.publicissapient.kpidashboard.apis.jira.model.BoardDetailsDTO;
+import com.publicissapient.kpidashboard.apis.jira.model.BoardRequestDTO;
+import com.publicissapient.kpidashboard.apis.jira.model.JiraBoardListResponse;
+import com.publicissapient.kpidashboard.apis.util.RestAPIUtils;
+import com.publicissapient.kpidashboard.common.model.connection.Connection;
+import com.publicissapient.kpidashboard.common.repository.connection.ConnectionRepository;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Service
+@Slf4j
+public class JiraToolConfigServiceImpl {
+
+	private static final String RESOURCE_JIRA_BOARD_ENDPOINT = "/rest/agile/1.0/board?projectKeyOrId=%s&startAt=%d&type=%s";
+
+	final ObjectMapper mapper = new ObjectMapper();
+
+	@Autowired
+	private RestTemplate restTemplate;
+
+	@Autowired
+	private RestAPIUtils restAPIUtils;
+
+	@Autowired
+	private ConnectionRepository connectionRepository;
+
+	public List<BoardDetailsDTO> getJiraBoardDetailsList(BoardRequestDTO boardRequestDTO) {
+
+		List<BoardDetailsDTO> responseList = new ArrayList<>();
+		Optional<Connection> optConnection = connectionRepository
+				.findById(new ObjectId(boardRequestDTO.getConnectionId()));
+		if (optConnection.isPresent()) {
+			Connection connection = optConnection.get();
+			String baseUrl = connection.getBaseUrl() == null ? null : connection.getBaseUrl().trim();
+			HttpEntity<?> httpEntity = getHttpEntity(connection);
+			fetchBoardDetailsRestAPICall(boardRequestDTO, responseList, baseUrl, httpEntity);
+			return responseList;
+		}
+		return responseList;
+	}
+
+	private void fetchBoardDetailsRestAPICall(BoardRequestDTO boardRequestDTO, List<BoardDetailsDTO> responseList,
+			String baseUrl, HttpEntity<?> httpEntity) {
+		long startAt = 0;
+		long nextPageIndex = startAt;
+		boolean isLast = false;
+		do {
+			try {
+				String url = String.format(new StringBuilder(baseUrl).append(RESOURCE_JIRA_BOARD_ENDPOINT).toString(),
+						boardRequestDTO.getProjectKey(), nextPageIndex, boardRequestDTO.getBoardType());
+
+				ResponseEntity<String> response = restTemplate.exchange(url, HttpMethod.GET, httpEntity, String.class);
+
+				if (response.getStatusCode() == HttpStatus.OK) {
+					JiraBoardListResponse jiraBoardListResponse = mapper.readValue(response.getBody(),
+							JiraBoardListResponse.class);
+					if (!jiraBoardListResponse.getIsLast().equalsIgnoreCase("true")) {
+						nextPageIndex = jiraBoardListResponse.getStartAt() + jiraBoardListResponse.getMaxResults();
+						isLast = true;
+					} else {
+						isLast = false;
+					}
+					setBoardListResponse(responseList, jiraBoardListResponse);
+				} else {
+					String statusCode = response.getStatusCode().toString();
+					log.error("Error while fetching BoardList from {}. with status {}", url, statusCode);
+				}
+
+			} catch (Exception exception) {
+				log.error("Error while fetching boardList for projectKey Id {}:  {}", boardRequestDTO.getProjectKey(),
+						exception.getMessage());
+			}
+		} while (isLast);
+	}
+
+	private void setBoardListResponse(List<BoardDetailsDTO> responseList, JiraBoardListResponse jiraBoardListResponse) {
+		jiraBoardListResponse.getValues().stream().forEach(jiraBoardValueResponse -> {
+			BoardDetailsDTO boardDetailsDTO = new BoardDetailsDTO();
+			boardDetailsDTO.setBoardId(jiraBoardValueResponse.getId());
+			boardDetailsDTO.setBoardName(jiraBoardValueResponse.getName());
+			responseList.add(boardDetailsDTO);
+		});
+	}
+
+	private HttpEntity<?> getHttpEntity(Connection connection) {
+		String username = connection.getUsername() == null ? null : connection.getUsername().trim();
+		String password = connection.getPassword() == null ? null
+				: restAPIUtils.decryptPassword(connection.getPassword());
+		HttpHeaders headers = restAPIUtils.getHeaders(username, password);
+		return new HttpEntity<>(headers);
+	}
+}
