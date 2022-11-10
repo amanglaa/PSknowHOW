d46ac98eace1a246e6dcfd3ae40cf5c890733bfa
narsingh9 <narender.singhnegi@publicissapient.com>
2022-11-10 16:11:19 +0530

    removed abondened stories

--- customapi/src/test/java/com/publicissapient/kpidashboard/apis/jira/scrum/service/DSRServiceImplTest.java
+++ customapi/src/test/java/com/publicissapient/kpidashboard/apis/jira/scrum/service/DSRServiceImplTest.java
@@ -187,7 +187,7 @@ public class DSRServiceImplTest {
 		Map<String, Object> defectDataListMap = dsrServiceImpl.fetchKPIDataFromDb(leafNodeList, startDate, endDate,
 				kpiRequest);
 		assertThat("Total Defects value :", ((List<JiraIssue>) (defectDataListMap.get(TOTALBUGKEY))).size(),
-				equalTo(0));
+				equalTo(2));
 	}
 
 	@Test
